abb604fd8896e04a7b4168310fb8cdb4762cbbb9
Craig Walls <craig@habuma.com>
2024-05-22 19:34:06 -0600

    Update Gemma example to use fluent ChatClient

--- spring-ai-gemma/src/main/java/habuma/springaigemma/SpringAiGemmaApplication.java
+++ spring-ai-gemma/src/main/java/habuma/springaigemma/SpringAiGemmaApplication.java
@@ -1,6 +1,7 @@
 package habuma.springaigemma;
 
 import org.springframework.ai.chat.ChatClient;
+import org.springframework.ai.chat.ChatModel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -16,12 +17,21 @@ public class SpringAiGemmaApplication {
     }
 
     @Bean
+    ChatClient chatClient(ChatModel chatModel) {
+        return ChatClient.builder(chatModel).build();
+    }
+
+    @Bean
     RouterFunction<ServerResponse> routes(ChatClient chatClient) {
         return RouterFunctions.route()
-            .GET("/ask", req ->
-                ServerResponse.ok().body(
-                    chatClient.call(req.param("question")
-                            .orElse("tell me a joke"))))
+            .GET("/ask", req -> {
+                String answer = chatClient.prompt()
+                    .user(req.param("question").orElse("tell me a joke"))
+                    .call()
+                    .content();
+
+                return ServerResponse.ok().body(answer);
+            })
             .build();
     }
 
