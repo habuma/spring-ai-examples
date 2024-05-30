6eb96cb23fad2413ad5023ea87f9784c48e7d7cb
Craig Walls <craig@habuma.com>
2024-05-29 23:12:36 -0600

    Update to Spring AI 1.0.0-M1

--- prompts-and-output-parsers/src/main/java/habuma/springaiessentialexample/JokeController.java
+++ prompts-and-output-parsers/src/main/java/habuma/springaiessentialexample/JokeController.java
@@ -2,6 +2,7 @@ package habuma.springaiessentialexample;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,21 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class JokeController {
 
-    private String promptTemplate;
-
     private final ChatClient chatClient;
 
-    public JokeController(ChatClient.Builder chatClientBuilder,
-                          @Value("${app.promptTemplate}") String promptTemplate) {
+    @Value("classpath:/joke-template.st")
+    private Resource jokeTemplate;
+
+    public JokeController(ChatClient.Builder chatClientBuilder) {
         this.chatClient = chatClientBuilder.build();
-        this.promptTemplate = promptTemplate;
     }
 
     @GetMapping("/joke")
     public JokeResponse tellJoke(@RequestParam("subject") String subject) {
         return chatClient.prompt()
             .user(userSpec -> userSpec
-                .text(promptTemplate)
+                .text(jokeTemplate)
                 .param("subject", subject))
             .call()
             .entity(JokeResponse.class);
