8f8b79e84e12c61753575ef1c0100e895fd72cb4
Craig Walls <craig@habuma.com>
2024-09-21 09:37:34 -0600

    Update RAG example to use QuestionAnswerAdvisor

--- spring-ai-rag-example/src/main/java/com/example/springairag/AskController.java
+++ spring-ai-rag-example/src/main/java/com/example/springairag/AskController.java
@@ -1,13 +1,8 @@
 package com.example.springairag;
 
-import java.util.List;
-
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.document.Document;
-import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
 import org.springframework.ai.vectorstore.VectorStore;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,10 +15,6 @@ public class AskController {
     private final ChatClient aiClient;
     private final VectorStore vectorStore;
 
-    @Value("classpath:/rag-prompt-template.st")
-    private Resource ragPromptTemplate;
-
-
     public AskController(ChatClient aiClient, VectorStore vectorStore) {
         this.aiClient = aiClient;
         this.vectorStore = vectorStore;
@@ -31,18 +22,9 @@ public class AskController {
 
     @PostMapping
     public Answer ask(@RequestBody Question question) {
-        List<Document> similarDocuments = vectorStore
-            .similaritySearch(SearchRequest.query(question.question())
-            .withTopK(2));
-        List<String> contentList = similarDocuments.stream()
-            .map(Document::getContent)
-            .toList();
-
         String answer = aiClient.prompt()
-            .user(userSpec -> userSpec
-                .text(ragPromptTemplate)
-                .param("input", question.question())
-                .param("documents", String.join("\n", contentList)))
+            .user(question.question())
+            .advisors(new QuestionAnswerAdvisor(vectorStore))
             .call()
             .content();
 
