9300140647da3220a4166186a820a888ac1d3155
Craig Walls <craig@habuma.com>
2025-05-20 15:31:35 -0600

    Upgrade Kotlin RAG example to Spring AI 1.0.0

--- spring-ai-kotlin-rag/src/main/kotlin/com/example/demo/AskController.kt
+++ spring-ai-kotlin-rag/src/main/kotlin/com/example/demo/AskController.kt
@@ -1,6 +1,6 @@
 package com.example.demo
 import org.springframework.ai.chat.client.ChatClient
-import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.web.bind.annotation.PostMapping
@@ -13,7 +13,8 @@ data class Answer(val answer: String?)
 @RestController
 class AskController(chatClientBuilder: ChatClient.Builder, vectorStore: VectorStore) {
     private val chatClient = chatClientBuilder
-        .defaultAdvisors(QuestionAnswerAdvisor(vectorStore, SearchRequest.builder().build()))
+        .defaultAdvisors(
+            QuestionAnswerAdvisor.builder(vectorStore).build())
         .build()
 
     @PostMapping("/ask")
