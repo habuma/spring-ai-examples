818cf070e143683895e69f948e9e01358d4b38f8
Craig Walls <craig@habuma.com>
2025-05-20 15:17:43 -0600

    Upgrade evaluator example to Spring AI 1.0.0

--- spring-ai-evaluators/src/main/java/com/example/springaievaluators/GameRulesService.java
+++ spring-ai-evaluators/src/main/java/com/example/springaievaluators/GameRulesService.java
@@ -1,7 +1,7 @@
 package com.example.springaievaluators;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Service;
--- spring-ai-evaluators/src/test/java/com/example/springaievaluators/EvaluatorTests.java
+++ spring-ai-evaluators/src/test/java/com/example/springaievaluators/EvaluatorTests.java
@@ -2,14 +2,13 @@ package com.example.springaievaluators;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
+import org.springframework.ai.chat.evaluation.RelevancyEvaluator;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.evaluation.EvaluationRequest;
 import org.springframework.ai.evaluation.EvaluationResponse;
-import org.springframework.ai.evaluation.RelevancyEvaluator;
-import org.springframework.ai.model.Content;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
